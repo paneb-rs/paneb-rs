0eebe0852ef9183ab399a296f59fc19bfbfc04aa
Valentin Fries <contact@fries.io>
2018-03-05 16:41:41 +0100

    Simple test model

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,41 @@
 extern crate nalgebra;
 
+pub trait Model<Input, Output> {
+	fn train(&self, input: Input, output: Output);
+	fn compute(&self, input: Input) -> Output;
+}
+
+pub struct TestModel {}
+
+impl TestModel {
+	
+	pub fn new() -> Self {
+		TestModel{}
+	}
+}
+
+impl Model<f64, f64> for TestModel {
+	
+	fn train(&self, _input: f64, _output: f64) {
+		// TODO: train model
+	}
+	
+	fn compute(&self, input: f64) -> f64 {
+		input + 40.
+	}
+}
+
 #[no_mangle]
-pub extern fn super_add(value: f64) -> f64 {
-	value + 42.
+pub unsafe extern fn test_model_create() -> *const TestModel {
+	&TestModel::new() as *const TestModel
+}
+
+#[no_mangle]
+pub unsafe extern fn test_model_compute(model: *const TestModel, input: f64) -> f64{
+	(*model).compute(input)
 }
 
 #[cfg(test)]
 mod test {
-	use super_add;
-	
-	#[test]
-	fn test() {
-		assert_eq!(49., super_add(7.));
-	}
+
 }
